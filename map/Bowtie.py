8d32ca7a56f64c2fdd8dd9e4547d8fcf7374d721
Gregor Rot <gregor.rot@gmail.com>
2012-02-08 20:55:43 +0100

    Added bowtie pair-end mapping support.

--- map/Bowtie.py
+++ map/Bowtie.py
@@ -1,6 +1,7 @@
 import biox
 from os.path import join as pjoin
 import os
+import sys
 
 class Bowtie():
 
@@ -89,18 +90,27 @@ class Bowtie():
         sam_files = []
         stat_files = []
         un_files = []
+        bam_files = []
         if self.trim3==False:
             stats_par = "%s.stats" % (output) if stats==True else "/dev/null" 
-            input_par = input
+            if type(input)==list:
+                if len(input)==2:
+                    input_par = "-1 %s -2 %s" % (input[0], input[1])
+                else:
+                    input_par = input[0]
+            else:
+                input_par = input
             output_par = "%s.sam" % output
             un_par = "--un "+output+".unmapped" if self.un_enabled else ""
             max_par = "--max "+output+".maxmulti" if self.max_enabled else ""
             command = "{bowtie_exec} {un_par} {max_par} {n_par} {v_par} {sam_par} {m_par} {index_par} {fasta_par} {input_par} 1>{output_par} 2>{output_stats}".format \
             (bowtie_exec = self.bowtie_exec, fasta_par = fasta_par, index_par = index_par, input_par = input_par, output_par = output_par, \
-            sam_par = sam_par, n_par = n_par, v_par = v_par, output_stats = stats_par, m_par = m_par)
+            sam_par = sam_par, n_par = n_par, v_par = v_par, output_stats = stats_par, m_par = m_par, \
+            un_par = un_par, max_par = max_par)
             str, err = biox.utils.cmd(command)
             sam_files.append(output_par)
-            un_files.append(output+".unmapped")
+            if self.un_enabled:
+                un_files.append(output+".unmapped")
             if bam: # create bam file
                 bam_output = "%s.bam" % output
                 command = "{samtools_exec} view -F 4 -bS {sam} > {bam}".format(samtools_exec = self.samtools_exec, sam = output_par, bam = bam_output)
@@ -114,17 +124,33 @@ class Bowtie():
                 f.write("%s\t%s\n" % (reads, mapped))
                 f.close()
         else:
-            bam_files = []
             reads = {}
             mapped = {}
             for t in range(0, self.trim3_iter+1):
                 trim3 = t * self.trim3_step
                 stats_par = "%s.trim%s.stats" % (output, trim3) if stats==True else "/dev/null" 
+                
                 if t==0:
-                    input_par = input
+                    if type(input)==list:
+                        if len(input)==2:
+                            input_par = "-1 %s -2 %s" % (input[0], input[1])
+                        elif len(input)==1:
+                            input_par = input[0]
+                    else:
+                        input_par = input
                 else:
-                    input_par = un_par
-                un_par = output+".trim%s.unmapped" % trim3 # obligatory when doing trimming
+                    if type(input)==list:
+                        if len(input)==2:
+                            input_par = "-1 %s_1 -2 %s_2" % (un_par, un_par)
+                    else:
+                        input_par = un_par
+                        
+                # use _ instead of . here because pair-end mapping naming of
+                # bowtie is not working correctly when using dot
+                # eg.: output.trim0.unmapped -> output.trim0_1.unmapped, output.trim0_2.unmapped
+                # instead of output.trim0.unmapped_1, output.trim0.unmapped_2
+                un_par = output+"_trim%s_unmapped" % trim3
+                    
                 max_par = "--max "+output+".trim%s.maxmulti" % trim3 if self.max_enabled else ""
                 trim3_par = "--trim3 %s" % trim3
                 output_par = output+".trim%s.sam" % trim3
@@ -134,7 +160,14 @@ class Bowtie():
                 un_par = un_par, max_par = max_par)
                 str, err = biox.utils.cmd(command)
                 sam_files.append(output_par)
-                un_files.append(un_par)
+                if type(input)==list:
+                    if len(input)==2:
+                        un_files.append("%s_1" % un_par)
+                        un_files.append("%s_2" % un_par)
+                    elif len(input)==1:
+                        un_files.append(un_par)
+                else:
+                        un_files.append(un_par)
                 if bam:
                     bam_file = output_par[:-3]+"bam"
                     command = "{samtools_exec} view -F 4 -bS {sam} > {bam}".format(samtools_exec = self.samtools_exec, sam = output_par, bam = bam_file)
@@ -148,7 +181,6 @@ class Bowtie():
                 bam_output = "%s.bam" % output
                 command = "{samtools_exec} merge -h {header_sam} {bam_output} {bam_files}".format(samtools_exec = self.samtools_exec, \
                 header_sam = output_par, bam_output = bam_output, bam_files = " ".join(bam_files))
-                print command
                 str, err = biox.utils.cmd(command)
             # create trim statistics
             trims = reads.keys()
@@ -160,6 +192,15 @@ class Bowtie():
                 f.write("%s\t%s\t%s\n" % (trim, reads.get(trim, 0), mapped.get(trim, 0)))
             f.close()
                 
+        if bam: # sort and index bam file
+            bam_sorted = bam_output[:-4]+"_sorted"
+            command = "{samtools_exec} sort {bam} {bam_sorted}".format(samtools_exec = self.samtools_exec, bam = bam_output, bam_sorted = bam_sorted)
+            str, err = biox.utils.cmd(command)
+            command = "{samtools_exec} index {bam_sorted}".format(samtools_exec = self.samtools_exec, bam_sorted = bam_sorted+".bam")
+            str, err = biox.utils.cmd(command)
+            os.rename(bam_sorted+".bam", output+".bam")
+            os.rename(bam_sorted+".bam.bai", output+".bam.bai")
+
         if delete_temp:
             for stat_file in stat_files:
                 os.remove(stat_file)
@@ -171,14 +212,6 @@ class Bowtie():
         if delete_temp and self.un_enabled in [False, None]:
             for un_file in un_files:
                 os.remove(un_file)
-        if bam: # sort and index bam file
-            bam_sorted = bam_output[:-4]+"_sorted"
-            command = "{samtools_exec} sort {bam} {bam_sorted}".format(samtools_exec = self.samtools_exec, bam = bam_output, bam_sorted = bam_sorted)
-            str, err = biox.utils.cmd(command)
-            command = "{samtools_exec} index {bam_sorted}".format(samtools_exec = self.samtools_exec, bam_sorted = bam_sorted+".bam")
-            str, err = biox.utils.cmd(command)
-            os.rename(bam_sorted+".bam", output+".bam")
-            os.rename(bam_sorted+".bam.bai", output+".bam.bai")
         return True
         
     def make_index(self, fasta, index_name):
