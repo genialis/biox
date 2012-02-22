45ec4235fbe0645469d76c0526e47426e19fedf5
rogrro <unknown>
2012-02-22 14:18:12 +0100

    Added data.fastq_qminmax(filename).

--- data/__init__.py
+++ data/__init__.py
@@ -4,3 +4,15 @@ import biox
 from TabReader import *
 from Fastq import *
 from Fasta import *
+
+"""
+Returns min and max quality ord value from fastq file.
+"""
+def fastq_qminmax(filename):
+    qmin = ()
+    qmax = 0
+    f = biox.data.Fastq(filename)
+    while f.read():
+        qmin = min(qmin, ord(f.quality[-1]))
+        qmax = max(qmax, ord(f.quality[0]))        
+    return qmin, qmax
