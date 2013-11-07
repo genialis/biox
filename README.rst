149fc9e7840c641285b63754a8fe82dd1ba2bbf1
Miha Stajdohar <miha.stajdohar@gmail.com>
2013-11-07 12:03:16 +0100

    Created Python package.

---
+++ README.rst
@@ -0,0 +1,20 @@
+BIOx
+====
+
+A Python library which `PIPA <http://pipa.biolab.si>`_ uses to perform bioinformatics analysis.
+
+Installation
+------------
+
+To install run::
+
+    python setup.py install
+
+Installation for Developers
+---------------------------
+
+To install in `development mode`_ run::
+
+    python setup.py develop
+
+.. _development mode: http://packages.python.org/distribute/setuptools.html#development-mode
---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+NAME = "BIOx"
+VERSION = "0.1"
+
+DESCRIPTION = "A Python library which PIPA uses to perform bioinformatics analysis."
+LONG_DESCRIPTION = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+AUTHOR = "Bioinformatics Laboratory at University of Ljubljana"
+AUTHOR_EMAIL = "marko.toplak@fri.uni-lj"
+URL = "https://bitbucket.org/markotoplak/biox"
+LICENSE = "Proprietary software"
+
+if __name__ == '__main__':
+    setup(
+        name=NAME,
+        version=VERSION,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        url=URL,
+        license=LICENSE,
+        packages=['biox'],
+        package_data={},
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'License :: Other/Proprietary License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+        ],
+        include_package_data=True,
+        zip_safe=False,
+        install_requires=[],
+    )
